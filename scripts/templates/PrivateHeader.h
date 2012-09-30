8be51ca9b8e45436b32f5442548e679869e76538
Tyler Heck <tyler.heck@gmail.com>
2012-09-30 17:06:51 -0400

    Update: private header template removes outdated init
    Old initialization method removed from scripts/templates/PrivateHeader.h

--- scripts/templates/PrivateHeader.h
+++ scripts/templates/PrivateHeader.h
@@ -25,11 +25,6 @@ struct %CODECLASSNAME%_struct{
  * Add additional arguments as needed */
 %CODECLASSNAME% * createDefault%CODECLASSNAME%(void);
 
-/* function performs all initialization on new instances %CODECLASSNAME%, MUST
- * BE CALLED IMMEDIATELY AFTER INSTANTIATION. This function may be called by a
- * default constructor that all other constructors build off of */
-void init%CODECLASSNAME%Base(%CODECLASSNAME% *to_init);
-
 /* deallocator, frees instance of class back to memory. Should not be called
  * manually, instance will be destroyed when reference count reaches 0 */
 void dealloc%CODECLASSNAME%(obj *to_dealloc);
