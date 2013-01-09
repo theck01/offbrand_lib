c56c08e8965602fb4b3cb41b3cebcbcd35aa7dd0
Tyler Heck <tyler.heck@gmail.com>
2013-01-08 21:13:27 -0500

    Update: Class source template compilation, documentation, asserts

--- scripts/templates/Source.c
+++ scripts/templates/Source.c
@@ -9,7 +9,50 @@
 
 /* PUBLIC METHODS */
 
-/* function can be deleted if unneeded */
+
+/* DEFINE PUBLIC METHODS HERE */
+
+
+/* PRIVATE METHODS */
+
+/* add arguments to complete initialization as needed, modify 
+ * %CODECLASSNAME%_Private.h as well if modifications are made */
+%CODECLASSNAME% * createDefault%CODECLASSNAME%(void){
+
+  static const char classname[] = "%CODECLASSNAME%";
+  %CODECLASSNAME% *new_instance = malloc(sizeof(%CODECLASSNAME%));
+  assert(new_instance != NULL);
+
+  /* initialize base class data */
+  initBase((obj *)new_instance, &dealloc%CODECLASSNAME%, &hash%CODECLASSNAME%,
+           &compare%CODECLASSNAME%s, classname);
+
+  /* ADD CLASS SPECIFIC INITIALIZATION HERE */
+
+  return new_instance;
+}
+
+
+obhash_t hash%CODECLASSNAME%(const obj *to_hash){
+
+  obhash_t value = 0;
+  %CODECLASSNAME% *instance = (%CODECLASSNAME% *)to_hash;
+
+  assert(to_hash);
+  assert(objIsOfClass(to_hash, "%CODECLASSNAME%"));
+
+  /* Implement a hash function suitable for uniquely itentifying
+   * %CODECLASSNAME% instances if default hash is not adequate. If no
+   * hash function is desired then be sure to pass NULL as the hash argument
+   * for initBase in createDefault%CODECLASSNAME% to use the default hash 
+   * function. Returning the stdlib hash function here can cause concurrency
+   * issues in C versions less than C11 (even though it will work for single 
+   * thread programs). If unneeded and above considerations are met then
+   * this function can be deleted from all %CODECLASSNAME% Files*/
+  return 0;
+}
+
+
 int8_t compare%CODECLASSNAME%s(const obj *a, const obj *b){
   
   const %CODECLASSNAME% *comp_a = (%CODECLASSNAME% *)a;  
@@ -32,43 +75,6 @@ int8_t compare%CODECLASSNAME%s(const obj *a, const obj *b){
 }
 
 
-/* DEFINE ADDITIONAL PUBLIC METHODS HERE */
-
-
-/* PRIVATE METHODS */
-
-/* add arguments to complete initialization as needed, modify 
- * %CODECLASSNAME%_Private.h as well if modifications are made */
-%CODECLASSNAME% * createDefault%CODECLASSNAME%(void){
-
-  static const char classname[] = "%CODECLASSNAME%";
-  %CODECLASSNAME% *new_instance = malloc(sizeof(%CODECLASSNAME%));
-  assert(new_instance != NULL);
-
-  /* initialize base class data */
-  initBase((obj *)new_instance, &dealloc%CODECLASSNAME%, &hash%CODECLASSNAME%,
-           &compare%CODECLASSNAME%, classname);
-
-  /* ADD CLASS SPECIFIC INITIALIZATION HERE */
-
-  return new_instance;
-}
-
-/* function can be deleted if unneeded */
-obhash_t hash%CODECLASSNAME%(const obj *to_hash){
-
-  /* Implement a hash function suitable for uniquely itentifying
-   * %CODECLASSNAME% instances if default hash is not adequate. If no
-   * hash function is desired then be sure to pass NULL as the hash argument
-   * for initBase in createDefault%CODECLASSNAME% to use the default hash 
-   * function. Returning the stdlib hash function here can cause concurrency
-   * issues in C versions less than C11 (even though it will work for single 
-   * thread programs). If unneeded and above considerations are met then
-   * this function can be deleted from all %CODECLASSNAME% Files*/
-  return 0;
-}
-
-
 void dealloc%CODECLASSNAME%(obj *to_dealloc){
 
   /* cast generic obj to %CODECLASSNAME% */
@@ -78,7 +84,7 @@ void dealloc%CODECLASSNAME%(obj *to_dealloc){
   assert(objIsOfClass(to_dealloc, "%CODECLASSNAME%"));
 
   /* PERFORM CLASS SPECIFIC MEMORY MANAGEMENT ON instance HERE BUT DO NOT
-   * FREE INSTANCE, THE LIBRARY WILL DO THIS FOR THE USER */
+   * FREE INSTANCE, THE LIBRARY WILL DO THAT */
 
   return;
 }
