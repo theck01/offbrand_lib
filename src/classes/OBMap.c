8d1b73310fe01711794ae5661b55c5cc821da31f
Tyler Heck <tyler.heck@gmail.com>
2013-01-24 16:09:27 -0500

    Add: OBMap implementation and tests

--- src/classes/OBMap.c
+++ src/classes/OBMap.c
@@ -13,11 +13,43 @@
 /* DEFINE PUBLIC METHODS HERE */
 
 
-/* PRIVATE METHODS */
+/* OBMapPair PRIVATE METHODS */
 
-/* add arguments to complete initialization as needed, modify 
- * OBMap_Private.h as well if modifications are made */
-OBMap * createDefaultMap(void){
+OBMap * createMapPair(obj *key, obj *value){
+
+  static const char classname[] = "OBMapPair";
+  OBMap *new_instance = malloc(sizeof(OBMapPair));
+  assert(new_instance != NULL);
+
+  /* initialize base class data */
+  initBase((obj *)new_instance, &deallocMapPair, NULL, NULL, classname);
+
+  retain(key);
+  new_instance->key = key;
+  retain(value);
+  new_instance->value = value;
+
+  return new_instance;
+}
+
+
+void deallocMapPair(obj *to_dealloc){
+
+  /* cast generic obj to OBMap */
+  OBMap *instance = (OBMapPair *)to_dealloc;
+
+  assert(to_dealloc);
+  assert(objIsOfClass(to_dealloc, "OBMapPair"));
+
+  release((obj *)instance->key);
+  release((obj *)instance->value);
+
+  return;
+}
+  
+
+
+OBMap * createDefaultMap(uint8_t cap_idx){
 
   static const char classname[] = "OBMap";
   OBMap *new_instance = malloc(sizeof(OBMap));
@@ -27,7 +59,10 @@ OBMap * createDefaultMap(void){
   initBase((obj *)new_instance, &deallocMap, &hashMap,
            &compareMaps, classname);
 
-    
+  new_instance->hash_table = createVector(MAP_CAPACITIES[cap_idx]);
+  new_instance->pairs = createDeque();
+  new_instance->cap_idx = cap_idx;
+  new_instance->collisions = 0;
 
   return new_instance;
 }
@@ -39,6 +74,7 @@ obhash_t hashMap(const obj *to_hash){
   static obhash_t seed = 0;
 
   obhash_t value;
+  OBDequeIterator *it;
   OBMap *instance = (OBMap *)to_hash;
 
   assert(to_hash);
@@ -52,38 +88,33 @@ obhash_t hashMap(const obj *to_hash){
 
   value = seed;
 
-  /* Implement a hash function suitable for uniquely itentifying
-   * OBMap instances if default hash is not adequate. If no
-   * hash function is desired then be sure to pass NULL as the hash argument
-   * for initBase in createDefaultOBMap to use the default hash 
-   * function. Returning the stdlib hash function here can cause concurrency
-   * issues in C versions less than C11 (even though it will work for single 
-   * thread programs). If unneeded and above considerations are met then
-   * this function can be deleted from all OBMap Files*/
+  it = getDequeHeadIt(instance->pairs);
+  if(!it) return value;
 
-  return 0;
+  /* perform commutative hash, so order of addition to table does not matter */
+  do{
+    value ^= hash(objAtDequeIt(instance->pairs, it));
+  }while(iterateDequeNext(instance->pairs, it));
+
+  release((obj *)it);
+
+  value += value << 3;
+  value ^= value >> 11;
+  value += value << 15;
+  
+  return value;
 }
 
 
 int8_t compareMaps(const obj *a, const obj *b){
   
-  const OBMap *comp_a = (OBMap *)a;  
-  const OBMap *comp_b = (OBMap *)b;  
-
   assert(a);
   assert(b);
   assert(objIsOfClass(a, "OBMap"));
   assert(objIsOfClass(b, "OBMap"));
 
-  /* add specific comparison logic, following the description in the header
-   * file. If no comparison function is desired then be sure to pass NULL to
-   * the compare argument of initBase in the createDefaultOBMap
-   * function to use the default pointer comparision Returning the stdlib 
-   * compare function here can cause concurrency issues in C versions less than
-   * C11 (even though it will work for single thread programs) If unneeded and 
-   * above considerations are met then this function can be deleted from all 
-   * OBMap Files*/
-  return OB_EQUAL_TO;
+  if(hash(a) == hash(b)) return OB_EQUAL_TO;
+  return OB_NOT_EQUAL;
 }
 
 
@@ -95,10 +126,9 @@ void deallocMap(obj *to_dealloc){
   assert(to_dealloc);
   assert(objIsOfClass(to_dealloc, "OBMap"));
 
-  /* PERFORM CLASS SPECIFIC MEMORY MANAGEMENT ON instance HERE BUT DO NOT
-   * FREE INSTANCE, THE LIBRARY WILL DO THAT */
+  release((obj *)instance->hash_table);
+  release((obj *)instance->pairs);
 
   return;
 }
 
-/* DEFINE ADDITIONAL PRIVATE METHODS HERE */
--- src/tests/OBMap_test.c
+++ src/tests/OBMap_test.c
@@ -14,7 +14,10 @@
 int main (){
 
   /* ADD NECESSARY UNIT TESTS HERE */
+  OBMap *test_map;
+  OBTest *a, *b, *c, *d, *e, *f, *g;
 
+/*  test_map = createMap(); */
   printf("OBMap_test: TESTS PASSED\n");
   return 0;
 }
