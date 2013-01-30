90cf12b70d772bc23f84587f4e9873071e8069c0
Tyler Heck <tyler.heck@gmail.com>
2013-01-30 10:37:54 -0500

    Update: Improved OBMap comment format for Doxygen

--- include/OBMap.h
+++ include/OBMap.h
@@ -24,7 +24,7 @@ OBMap * createMap(void);
 
 /**
  * @brief Constructor, creates a new, empty OBMap instance with capacity given
- * @param capacity, Capacity required for OBMap instance
+ * @param capacity Capacity required for OBMap instance
  * @return Pointer to the newly created OBMap instance
  */
 OBMap *createMapWithCapacity(uint32_t capacity);
@@ -32,7 +32,7 @@ OBMap *createMapWithCapacity(uint32_t capacity);
 /**
  * @brief Copy constructor, creates a new OBMap with the exact same contents
  * of another OBMap
- * @param m, Pointer to an instance of OBMap to copy
+ * @param m Pointer to an instance of OBMap to copy
  * @return A copy of the provided OBMap
  */
 OBMap * copyMap(const OBMap *to_copy);
@@ -40,9 +40,9 @@ OBMap * copyMap(const OBMap *to_copy);
 /**
  * @brief Add a key-value pair to an OBMap
  *
- * @param m, Pointer to an instance of OBMap
- * @param key, Pointer to any Offbrand compatible class to use as a lookup key
- * @param key, Pointer to any Offbrand compatible class 
+ * @param m Pointer to an instance of OBMap
+ * @param key Pointer to any Offbrand compatible class to use as a lookup key
+ * @param value Pointer to any Offbrand compatible class
  *
  * @details If the key is already contained within the m then the old value
  * stored at that key is replaced with the new value
@@ -52,8 +52,8 @@ void addToMap(OBMap *m, obj *key, obj *value);
 /**
  * @brief Lookup the value stored at key in an OBMap
  *
- * @param m, Pointer to an instance of OBMap
- * @param key, Pointer to any Offbrand compatible class to use as a lookup key
+ * @param m Pointer to an instance of OBMap
+ * @param key Pointer to any Offbrand compatible class to use as a lookup key
  *
  * @retval NULL Key not found in OBMap instance or key bound to NULL value
  * @retval non-NULL Value found at key in OBMap
@@ -63,8 +63,8 @@ obj * lookupMapKey(const OBMap *m, const obj *key);
 /**
  * @brief Removes a key-value pair from an OBMap
  * 
- * @param m, Pointer to an instance of OBMap
- * @param key, Pointer to any Offbrand compatible class to use as a lookup key
+ * @param m Pointer to an instance of OBMap
+ * @param key Pointer to any Offbrand compatible class to use as a lookup key
  * 
  * @details If no key exists then the funciton will silently do nothing
  *
@@ -76,7 +76,7 @@ void removeMapKey(OBMap *m, obj *key);
 /**
  * @brief Rehashes all elements contained in an OBMap
  *
- * @param m, Pointer to an instance of OBMap
+ * @param m Pointer to an instance of OBMap
  *
  * @details This method is useful for ensuring that mutable keys will still lead
  * to valid lookups of the associated values even after key(s) have been altered
@@ -87,7 +87,7 @@ void rehashMap(OBMap *m);
  * @brief Clears an OBMap of all key-value pairs, essentially restoring it
  * to the initial empty state that existed after creation
  *
- * @param m, Pointer to an instance of OBMap
+ * @param m Pointer to an instance of OBMap
  */
 void clearMap(OBMap *m);
 
